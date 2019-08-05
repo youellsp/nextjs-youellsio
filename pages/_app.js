0c09322a5f672cdf9b0874bcda46427f642d50a0
youellsp <youells.patrick@gmail.com>
2019-08-04 21:57:24 -0400

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,42 @@
+import withRedux from 'next-redux-wrapper'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+import styledNormalize from 'styled-normalize'
+import { withRouter } from 'next/router'
+import { Provider } from 'react-redux'
+import App, { Container } from 'next/app'
+import Layout from 'components/Layout'
+import createStore from 'store/createStore'
+import theme from 'theme'
+
+const GlobalStyle = createGlobalStyle`
+  ${styledNormalize}
+`
+
+class MyApp extends App {
+  static async getInitialProps ({ Component, ctx }) {
+    return {
+      pageProps: Component.getInitialProps
+        ? await Component.getInitialProps(ctx)
+        : {}
+    }
+  }
+  render () {
+    const { Component, pageProps, store, router } = this.props
+    return (
+      <Container>
+        <ThemeProvider theme={theme}>
+          <Provider store={store}>
+            <GlobalStyle />
+            <Layout>
+              <Component router={router} {...pageProps} />
+            </Layout>
+          </Provider>
+        </ThemeProvider>
+      </Container>
+    )
+  }
+}
+
+export default withRedux(createStore)(
+  withRouter(MyApp)
+)
